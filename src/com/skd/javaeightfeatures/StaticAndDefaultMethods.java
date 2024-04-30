012e38c7517aba2d02104fb7cd4d6d29cb65b9a8
pavan kalyan <pavankalyanmrpk45@gmail.com>
2024-04-30 20:46:11 +0530

    lambda,methodreferences,consumer and static&default methods examples

---
+++ src/com/skd/javaeightfeatures/StaticAndDefaultMethods.java
@@ -0,0 +1,29 @@
+package com.skd.javaeightfeatures;
+
+interface DefaultAndStatic {
+/*
+ * in Java 8 Default & static methods are allowed inside interface.
+ */
+	default String getName(String name) {
+		return name;
+	}
+	
+	static int getAge(int age) {
+		return age;
+	}
+	
+}
+
+class DefaultAndStaticClass implements DefaultAndStatic{
+	
+}
+public class StaticAndDefaultMethods {
+	
+	public static void main(String[] args) {
+//		Up-casting :- Sub class object storing inside a Super class reference.
+		DefaultAndStatic object=new DefaultAndStaticClass();
+		
+		System.out.println("Name : "+object.getName("Pavan Kalyan ")+",Age : "+DefaultAndStatic.getAge(25));
+
+	}
+}
